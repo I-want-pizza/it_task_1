4b62b973eb6058f18f22b737ccb2b347a4b31856
romanpepper <wupsen1703@mail.ru>
2025-03-18 03:14:10 +0300

    added video

--- roman_paper_task_1/obj/Debug/net9.0/roman_paper_task_1.AssemblyInfo.cs
+++ roman_paper_task_1/obj/Debug/net9.0/roman_paper_task_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("roman_paper_task_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac7d56b969bdb388f207ba6ba7efaf0c7cd41af8")]
 [assembly: System.Reflection.AssemblyProductAttribute("roman_paper_task_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("roman_paper_task_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
